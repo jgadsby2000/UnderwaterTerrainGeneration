4e3f923ad564b78f5fee44ea599a38137f5452f7
Jack Gadsby <50556848+jgadsby2000@users.noreply.github.com>
2022-05-10 09:01:34 +0930

    building

---
+++ Assets/TerrainBuilder.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainBuilder : MonoBehaviour
+{
+    public static int xSize = 20;
+    public static int ySize = 10;
+    public static int zSize = 20;
+    public Vector3 Size = new Vector3(xSize, ySize, zSize);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
